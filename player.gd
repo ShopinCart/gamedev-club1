055fe5bd696f3d6bb84aaa596835989a2964bca8
ShopinCart <carteryxz@gmail.com>
2025-12-10 20:24:46 -0700

    feat: (Home) re did the movement

---
+++ player.gd
@@ -0,0 +1,22 @@
+extends Node2D
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	
+	var direction = Vector2.ZERO
+	var speed = 50.0
+	if Input.is_action_pressed("move_up"):
+		direction.y -= 1.0
+	if Input.is_action_pressed("move_down"):
+		direction.y += 1.0
+	if Input.is_action_pressed("move_left"):
+		direction.x -= 1.0
+	if Input.is_action_pressed("move_right"):
+		direction.x += 1.0
+
+	position += direction * (speed * delta)
